86d93a6eeb75dae5f71bab6df4aa9fec2e546691
Laura Lucero <107892806+laurilla86@users.noreply.github.com>
2022-09-21 16:04:11 -0300

    corrección entrega 15.09

--- Desafio.15.09/index.js
+++ Desafio.15.09/index.js
@@ -1,3 +1,4 @@
+function simulador() {
 alert("Bienvenido a mi tienda");
 let nombre = prompt("Ingrese nombre y apellido");
 let saludo = `Hola ${nombre} a continuación te vamos a mostrar los productos en oferta`;
@@ -5,8 +6,8 @@ alert(saludo);
 
 const ProductosEnOferta = [
     { producto: "remera básica", color: "rojo", talle: "S", precio: 6200},
-    { producto: "remera básica", color: "rojo", talle: "M", precio: 6200},
-    { producto: "remera básica", color: "rojo", talle: "L", precio: 6200},
+    { producto: "remera básica", color: "amarillo", talle: "M", precio: 6200},
+    { producto: "remera básica", color: "rosa", talle: "L", precio: 6200},
     { producto: "remera básica", color: "azul", talle: "M", precio: 6200},
     { producto: "remera básica", color: "verde", talle: "L", precio: 6200},
     { producto: "pantalón", color: "blanco", talle: "S", precio: 8000},
@@ -14,11 +15,12 @@ const ProductosEnOferta = [
 ]
 
 ProductosEnOferta.forEach(item => alert(item.producto +" " +item.color));
-let talleIngresado = prompt("Ingresa tu talle");
-
-let productos = ProductosEnOferta.filter(item => item.talle ===talleIngresado);
-alert(productos);
-
-
-
-
+let talleIngresado = prompt("Indicanos tu talle para verificar si tenemos stock. S-M-L");
+let resultadoTalle = ProductosEnOferta.filter(item => item.talle === talleIngresado);
+resultadoTalle.forEach(item => {
+    let mensaje = `
+    ${item.producto} ${item.color} $${item.precio}`;
+    alert(mensaje);
+})
+}
+simulador();
